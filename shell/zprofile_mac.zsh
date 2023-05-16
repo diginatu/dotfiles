aa836312a718384c694b68df93d9edebf6a81066
diginatu <diginatu.510o@gmail.com>
2023-05-16 21:47:46 +0900

    Remove mac files

--- shell/zprofile_mac.zsh
+++
@@ -1,8 +0,0 @@
-source ~/dotfiles/shell/zprofile
-
-# pyenv
-if [ -d "$HOME/.pyenv" ] ; then
-    export PYENV_ROOT="$HOME/.pyenv"
-    PATH="$PYENV_ROOT/bin:$PATH"
-    eval "$(pyenv init -)"
-fi
--- shell/zshrc_mac.zsh
+++
@@ -1,8 +0,0 @@
-source ~/dotfiles/shell/zshrc
-
-#alias ls='ls -G'
-#alias md5sum='md5'
-
-alias tp='trash'
-
-RPROMPT=''
