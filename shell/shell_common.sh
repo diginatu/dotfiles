86d61d47969274ce4443f16c7978960d95fee39c
diginatu <diginatu.510o@gmail.com>
2023-10-27 01:06:13 +0900

    shell: hyperlink

--- shell/shell_common.sh
+++ shell/shell_common.sh
@@ -1,5 +1,5 @@
 # aliases
-alias ls='ls --color=auto'
+alias ls='ls --color=auto --hyperlink=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
