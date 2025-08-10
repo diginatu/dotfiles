9c9478701aaf23704881002759f41709ed0bec64
diginatu <diginatu.510o@gmail.com>
2025-08-10 20:57:25 +0900

    Update for kitten remote command

--- terminal/kitty/auth_open_launch.py
+++ terminal/kitty/auth_open_launch.py
@@ -2,13 +2,16 @@ def is_cmd_allowed(pcmd, window, from_socket, extra_data):
     cmd_name = pcmd['cmd']
     cmd_payload = pcmd['payload']
 
-    if cmd_name != 'launch':
-        return None
-    args = cmd_payload.get('args')
-    if args and len(args) >= 2:
-        if args[0] == 'xdg-open' or args[0] == 'open':
-            print('Allowing launch command:', cmd_payload)
-            return True
+    print('Checking command:', cmd_name, cmd_payload)
 
-    print('Dening launch command:', cmd_payload)
+    if cmd_name == 'run':
+        args = cmd_payload.get('cmdline')
+        if not args:
+            return False
+        if args and (len(args) <= 2):
+            if args[0] == 'xdg-open' or args[0] == 'open':
+                print('Allowing open command:', cmd_payload)
+                return True
+
+    print('Dening command:', cmd_name, cmd_payload)
     return False
