5823d36f9c6a6b7fdaceed6daa7854245f218292
diginatu <diginatu.510o@gmail.com>
2025-09-19 00:19:49 +0900

    Kitty: Improve remote control

--- terminal/kitty/auth_open_launch.py
+++ terminal/kitty/auth_open_launch.py
@@ -12,6 +12,18 @@ def is_cmd_allowed(pcmd, window, from_socket, extra_data):
             if args[0] == 'xdg-open' or args[0] == 'open':
                 print('Allowing open command:', cmd_payload)
                 return True
+            if args[0] == 'remote-control-confirm':
+                print('Allowing remote-control-confirm command:', cmd_payload)
+                return True
+
+    if cmd_name == 'launch':
+        args = cmd_payload.get('args')
+        if not args:
+            return False
+        if args and (len(args) == 1):
+            if args[0].endswith('kitty-scrollback.nvim/python/loading.py'):
+                print('Allowing launch kitty-scrollback.nvim command:', cmd_payload)
+                return True
 
     print('Dening command:', cmd_name, cmd_payload)
     return False
